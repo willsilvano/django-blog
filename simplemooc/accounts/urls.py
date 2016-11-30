aee2ccae4ac067b9634187838576103c42b42d98
Willian Silvano Maria <willsilvano@gmail.com>
2016-11-30 21:49:07 -0200

    Changes layout

--- simplemooc/accounts/urls.py
+++ simplemooc/accounts/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
+from . import views
 
 urlpatterns = [
     url(r'^entrar/$', auth_views.login, {'template_name': 'accounts/login.html'}, name='login'),
-    # url(r'^registrar/$', auth_views.register, {'template_name': 'accounts/register.html'}, name='register'),
+    url(r'^registrar/$', views.register, name='register'),
 ]
