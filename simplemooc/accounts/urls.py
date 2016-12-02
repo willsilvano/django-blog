cbddec40af7ed3e648f4894c108e0a08e7e20915
Willian Silvano Maria <willsilvano@gmail.com>
2016-12-02 00:43:15 -0200

    Features for painel

--- simplemooc/accounts/urls.py
+++ simplemooc/accounts/urls.py
@@ -3,7 +3,9 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
+    url(r'^$', views.dashboard, name='dashboard'),
     url(r'^entrar/$', auth_views.login, {'template_name': 'accounts/login.html'}, name='login'),
     url(r'^sair/$', auth_views.logout, {'next_page': 'core:home'}, name='logout'),
     url(r'^cadastre-se/$', views.register, name='register'),
+    url(r'^editar/$', views.edit, name='edit'),
 ]
--- simplemooc/accounts/views.py
+++ simplemooc/accounts/views.py
@@ -1,6 +1,14 @@
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
-from .forms import RegisterForm
+from .forms import RegisterForm, EditAccountForm
+
+
+@login_required
+def dashboard(request):
+    template_name = 'accounts/dashboard.html'
+    return render(request, template_name)
+
 
 def register(request):
     template_name = 'accounts/register.html'
@@ -16,4 +24,20 @@ def register(request):
     context = {
         'form': form
     }
+    return render(request, template_name, context)
+
+
+@login_required
+def edit(request):
+    template_name = 'accounts/edit.html'
+    context = {}
+    if request.method == 'POST':
+        form = EditAccountForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            form = EditAccountForm(instance=request.user)
+            context['success'] = True
+    else:
+        form = EditAccountForm(instance=request.user)
+    context['form'] = form
     return render(request, template_name, context)
