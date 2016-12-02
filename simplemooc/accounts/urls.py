964663579f6aa27fd581a703bcefd14c6aa26f1c
Willian Silvano Maria <willsilvano@gmail.com>
2016-12-01 23:30:26 -0200

    Create logout feature

--- simplemooc/accounts/urls.py
+++ simplemooc/accounts/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^entrar/$', auth_views.login, {'template_name': 'accounts/login.html'}, name='login'),
-    url(r'^registrar/$', views.register, name='register'),
+    url(r'^sair/$', auth_views.logout, {'next_page': 'core:home'}, name='logout'),
+    url(r'^cadastre-se/$', views.register, name='register'),
 ]
--- simplemooc/accounts/views.py
+++ simplemooc/accounts/views.py
@@ -1,16 +1,18 @@
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
-from django.conf import settings
+from .forms import RegisterForm
 
 def register(request):
     template_name = 'accounts/register.html'
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = RegisterForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect(settings.LOGIN_URL)
+            user = form.save()
+            user = authenticate(username=user.username, password=form.cleaned_data['password1'])
+            login(request, user)
+            return redirect('core:home')
     else:
-        form = UserCreationForm()
+        form = RegisterForm()
     context = {
         'form': form
     }
